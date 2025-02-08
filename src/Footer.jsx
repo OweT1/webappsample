382f556ec46debf792527d53123f80f945cefbe3
OwenTanKL <113007459+OweT1@users.noreply.github.com>
2025-02-08 11:27:15 +0800

    Updated Github Project Link

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
       <div className="footer-content">
         <p>&copy; AY2024/25 DSA3101 Data Science in Practice Group 6</p>
         <a
-          href="https://github.com/your-github-repo"
+          href="https://github.com/OweT1/webappsample/"
           target="_blank"
           rel="noopener noreferrer"
         >
